d2816faebbb7579591af3109a50f65bbd32a46dc
Manas Sarpatwar <aca18mms@bessemer-node041.shef.ac.uk>
2021-03-25 19:45:08 +0000

    Custom data generator and CNN-LSTM model

--- processing/preprocess.py
+++ processing/preprocess.py
@@ -24,46 +24,49 @@ if not os.path.isdir(processed_data_dir):
 if not os.path.isdir(dataframe_dir):
     os.makedirs(dataframe_dir)
 
-def get_dataframe(feature, data, i):
+def get_dataframe(feature, data):
     saved_file_name = f"{dataframe_dir}/{feature}.h5"
     if os.path.isfile(saved_file_name):
-        print(f"--1.{i+1}--Using saved converted {feature}.h5")
+        print(f"--Using saved converted {feature}.h5")
         df = pd.read_hdf(saved_file_name, key="data")
     else:
-        print(f"--1.{i+1}--Converting {feature}")
+        print(f"--Converting {feature}")
         d = xr.open_dataset(f"{interpolated_data_dir}/{data['path']}")
         # Delete time_bnds index and column
         if 'time_bnds' in d.variables:
             d = d.drop('time_bnds')
         df = d.to_dataframe()
         del d
-        df.reset_index(inplace=True)
+        df = df.reset_index()
         print(df.head())
 
         # Rename variable
-        df.rename(columns={data['time']: 'start_date', data['name']: feature}, inplace=True)
+        df = df.rename(columns={data['time']: 'start_date', data['name']: feature})
+        print(df.shape)
+        df = df.drop_duplicates()
+        print(df.shape)
 
-        # Format time to start_date    
-        df['start_date'] = pd.to_datetime(df['start_date']).dt.strftime('%Y-%m-%d')
-        print(df.shape)
-        df.drop_duplicates(inplace=True)
-        print(df.shape)
+        # Format start_date to datetime column  
+        df['start_date'] = pd.to_datetime(df['start_date']).dt.date.astype('datetime64')
+
+        df = df.set_index('start_date')
+       
         print(df.head())
         df.to_hdf(saved_file_name, key="data")
     return df
 
-def get_merged_dataframe(merge=False):
+def get_merged_dataframe(merge=False, clean=True, save=True):
     saved_merged_file_name = f"{processed_data_dir}/processed_dataframe.h5"
     if not merge and os.path.isfile(saved_merged_file_name):
-        print("--1--Loading saved merged dataframe")
+        print("--Loading saved merged dataframe")
         merged_df = pd.read_hdf(saved_merged_file_name, key="data")
     else:
-        print("--1--Converting to dataframes and merging")
+        print("--Converting to dataframes and merging")
         merged_df = None
-        for i, (feature, data) in enumerate(features.items()):
-            df = get_dataframe(feature=feature, data=data, i=i)
+        for feature, data in features.items():
+            df = get_dataframe(feature=feature, data=data)
             if merged_df is not None:
-                print(f"     --Combining {feature} with {', '.join(list(merged_df.columns)[3:])}")
+                print(f"    --Combining {feature} with {', '.join(list(merged_df.columns)[3:])}")
                 merged_df = merged_df.merge(df, left_on=['start_date', 'lon', 'lat'], right_on=['start_date', 'lon', 'lat'], how='inner')
             else:
                 merged_df = df.copy()
@@ -71,18 +74,20 @@ def get_merged_dataframe(merge=False):
             del df
             print(merged_df.shape)
      
-        print("\n--2--Cleaning NaN values in merged dataframe")
-        # Setting nan values to mean
-        for i, feature in enumerate(features):
-            nan = merged_df[feature].isnull()
-            if nan.values.any():
-                print(f"--2.{i+1}--Found NaN values in column {feature}")
-                mean = merged_df[merged_df[feature].notnull()][feature].mean()
-                merged_df.loc[nan, feature] = mean
+        if clean:
+            print("\n--Cleaning NaN values in merged dataframe")
+            # Setting nan values to mean
+            for i, feature in enumerate(features):
+                nan = merged_df[feature].isnull()
+                if nan.values.any():
+                    print(f"--Found NaN values in column {feature}")
+                    mean = merged_df[merged_df[feature].notnull()][feature].mean()
+                    merged_df.loc[nan, feature] = mean
 
-        print("\n--3--Saving merged dataframe")
-        print(merged_df.head())
-        merged_df.to_hdf(saved_merged_file_name, key="data")
+        if save:
+            print("\n--Saving merged dataframe")
+            print(merged_df.head())
+            merged_df.to_hdf(saved_merged_file_name, key="data")
 
     return merged_df
 
@@ -94,28 +99,47 @@ def get_feature(feature, data):
 def get_normalized_dataframe(normalize=False):
     saved_normalized_file_name = f"{processed_data_dir}/processed_normalized_dataframe.h5"
     if not normalize and os.path.isfile(saved_normalized_file_name):
-        print("--1--Loading saved normalized dataframe")
+        print("--Loading saved normalized dataframe")
         df = pd.read_hdf(saved_normalized_file_name, key="data")
     else:
-        print("--1--Creating normalized dataframe")
+        print("--Creating normalized dataframe")
         df = get_merged_dataframe()
         scaler = MinMaxScaler()
         df[list(features)] = scaler.fit_transform(df[features])
-        print("\n--2--Saving normalized dataframe")
+        print("\n--Saving normalized dataframe")
         df.to_hdf(saved_normalized_file_name, key="data")
     return df
 
-def get_region_tensor(target_months=[6,7,8,9], horizon=56, lookback=26):
-    df = get_normalized_dataframe()
-    dates = df["start_date"].unique()
-    tensors = []
-    
-    for i, (feature, data) in enumerate(features.items()):
-        t, mask = get_feature(feature=feature, data=data)
-        tensors.append(t)
-        masks.append(mask)
-    return np.stack(tensors, axis=-1)
+def get_train_data(target_months=[6,7,8,9], horizon=56, lookback=26, features=['rf', 'temp', 'pres', 'slp', 'rhum'], target_feature='rf'):
+    train = get_normalized_dataframe()
+    # dates = df["start_date"].unique()
+    # train = train[train.index.shift(periods=(horizon+lookback), freq='D').month.isin(target_months)]
+    train = train.sort_values(by=['start_date', 'lat', 'lon'])
+    train_grid = (train['lat'].nunique(), train['lon'].nunique())
+    train = train[features]
 
+    saved_file_name = f"{dataframe_dir}/{target_feature}.h5"
+    print(f"--Using saved converted {target_feature}.h5")
+    target = pd.read_hdf(saved_file_name, key="data")
+
+    target = target[target.index.month.isin(target_months)]
+    target = target.sort_values(by=['start_date', 'lat', 'lon'])
+
+    # Drop na values
+    target = target.dropna()
+    saved_target_points_file_name = f"data/target_points_{target_feature}.csv"
+    if not os.path.isfile(saved_target_points_file_name):
+        target[['lat', 'lon']].drop_duplicates().to_csv(saved_target_points_file_name, index=None)
+        
+    target_shape = target.groupby(['lat', 'lon']).ngroups
+    
+    scaler = MinMaxScaler()
+    target[target_feature] = scaler.fit_transform(target[[target_feature]])
+
+    target = target[target_feature]
+
+    return train, target, train_grid, target_shape
+    
 if __name__ == '__main__':
     get_merged_dataframe()
 
--- train.py
+++ train.py
@@ -1,292 +1,143 @@
-"""
-Spatial-Temporal Models - temp34 Model
-***Note this script is adapted from a colab notebook to demonstrate required code for model construction in Tensorflow 2.0
-This script will not run as standalone***
-"""
-
 import numpy as np
-import datetime
-from math import floor
+import pandas as pd
+from processing.preprocess import get_train_data
 import tensorflow as tf
 print("tf version:",tf.__version__)
 print(tf.test.gpu_device_name())
-from tensorflow.keras.layers import Input, TimeDistributed, Dense, Conv2D, LSTM, Flatten, Reshape, Embedding
+
+from tensorflow import keras
+from tensorflow.keras.layers import Input, TimeDistributed, Dense, Conv2D, LSTM, Flatten, Reshape
 from tensorflow.keras.models import Model
-import tensorflow.keras.backend as K
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.losses import Huber, MeanAbsoluteError
-from tensorflow.keras.metrics import RootMeanSquaredError
+from tensorflow.keras.losses import Huber
+from tensorflow.keras.metrics import RootMeanSquaredError, mae
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-from utils.load_functions import load_locations, load_column_names
+from sklearn.utils import shuffle
+import wandb
+from wandb.keras import WandbCallback
 
-## Load Data ##
-global_region_tensor = np.load('data/processed/spatial_temporal/global_region_tensor_scaled_sg5.npy', allow_pickle=True)
-col_names = load_column_names()
-locations = load_locations()
-target_regions = locations['region_id']
+wandb.login()
 
-## Get Params ##
-# Rodeo Track Params
-SPRINT_WEEK = 21
-TAR_VAR = 'rf'
-TAR_WEEK = '34'
-PRED_TAR = TAR_VAR+TAR_WEEK
-TAR_INDEX = col_names.index(TAR_VAR)
-SEED = 112358
+TARGET_MONTHS = [6,7,8,9]
+TARGET_FEATURE = 'rf'
+HORIZON = 56 #56 days
+LOOKBACK = 26
+BATCH_SIZE = 32
+MODEL_NAME = f"{TARGET_FEATURE}-{HORIZON}-{LOOKBACK}"
+features=['rf', 'temp', 'pres', 'slp', 'rhum']
 
-# Data Params
-MAX_SG = 5 #maximum spatial granularity of local region from input data
-NUM_TIMESTEPS = global_region_tensor.shape[0]
-NUM_REGIONS = len(target_regions)
-NUM_FEATURES = global_region_tensor.shape[3]
-TEMPORAL_FEATURES = 4 #NUM_FEATURES-1 #remove date field
-SPATIAL_FEATURES = NUM_FEATURES-TEMPORAL_FEATURES-3 #remove date field, lat, lon, 4*time cyclic fields
-PADDED_LAT = global_region_tensor.shape[1]
-PADDED_LON = global_region_tensor.shape[2]
-TRAIN_SPLIT = NUM_TIMESTEPS - 2*1008
-VALID_SPLIT = NUM_TIMESTEPS - 1008
+train_years = [year for year in range(1979, 2018)]
+test_years = [year for year in range(train_years[-1], 2020)]
 
-# Tunable Processing Params
-SEQ_LEN = 26
-STEP = 7
-SG = 5
-SG = min(SG,MAX_SG) #check spatial granularity is less than maximum allowed by data
-# define target region ids
-target_region_ids = [(locations['lat'].max()-region[0]+MAX_SG,region[1]-locations['lon'].min()+MAX_SG) for region in target_regions]
-SPATIAL_WIDTH = 2*SG+1
-BATCH_SIZE = 256
-STEPS_PER_EPOCH = floor((NUM_REGIONS*TRAIN_SPLIT - ({'34':2,'56':4}[TAR_WEEK]*14 + 1) - SEQ_LEN*STEP)/BATCH_SIZE)
-BUFFER_SIZE = 15000
-PREFETCH_SIZE = 5
-EPOCHS = 50
+train, target, train_grid, target_shape = get_train_data(target_months=TARGET_MONTHS, target_feature=TARGET_FEATURE, 
+                                horizon=HORIZON, lookback=LOOKBACK, features=features)
 
-### Model Params
-# Architecture
-CONV2D_LAYERS = 1
-CONV1D_LAYERS = 0
-LSTM_LAYERS = 2
+X_train, y_train = train[train.index.year.isin(train_years)], target[target.index.year.isin(train_years)]
+X_test, y_test = train[train.index.year.isin(test_years)], target[target.index.year.isin(test_years)]
 
-# Embedding of region id
-RGN_ID_EMB_DIM = 8 # from 357 indian lat-lon regions
+class DataGenerator(keras.utils.Sequence):
+    'Generates data for Keras'
+    def __init__(self, train, target, lookback, horizon, train_grid, target_shape, batch_size, shuffle=True):
+        'Initialization'
+        self.train = train
+        self.target = target
+        self.target_dates = target.index.unique()
+        self.lookback = lookback
+        self.horizon = horizon
+        self.target_shape = target_shape
+        self.dim = (lookback, *train_grid, len(self.train.columns))
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.on_epoch_end()
 
-## Layer Params
-# Spatial conv layers
-SPTL_CONV_FILTERS = 16
-SPTL_KERNEL_SIZE = 2
-SPTL_STRIDE = 1
-SPTL_PADDING = 'same'
-SPTL_OUTPUT_DIM = 24 #12
-# Conv1D time dimensionality reduction layers
-CONV1D_FILTERS = 16
-# LSTM sequencing layers
-LSTM1_UNITS = 64
-LSTM2_UNITS = 64
-DROPOUT_RATE = 0.3
-REC_DROPOUT_RATE = 0 #else break GPU requirements https://www.tensorflow.org/api_docs/python/tf/keras/layers/GRU#used-in-the-notebooks_1
-FC1_UNITS = 128
-FC2_UNITS = 128
-MODEL_NAME = f"{PRED_TAR}_sp-tm_sg{str(SG)}_seq{str(SEQ_LEN)}_stp{str(STEP)}_emb{str(RGN_ID_EMB_DIM)}"
-TIMESTAMP = '{date:%Y_%m_%d_%H_%M}'.format(date = datetime.datetime.now())
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.floor(len(self.target_dates) / self.batch_size))
 
-# Define region id mapping
-rgn_id_vocab = [str(region) for region in target_regions]
-rgn_id_to_int = {rgn_id:i for i, rgn_id in enumerate(rgn_id_vocab)}
-int_to_rgn_id = {i:rgn_id for i, rgn_id in enumerate(rgn_id_vocab)}
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        batch_target_dates = self.target_dates[index*self.batch_size:(index+1)*self.batch_size]
 
-print("Prediction target: ",PRED_TAR,
-      "\nEpochs: ",EPOCHS,
-      "\nBatch size: ",BATCH_SIZE,
-      "\nSteps per epoch: ",STEPS_PER_EPOCH,
-      # "\nValidation steps: ",VALIDATION_STEPS,
-      "\nSequence length: ", SEQ_LEN,
-      "\nSpatial granularity: ", SG,
-      "\nMax spatial granularity: ", MAX_SG,
-      "\nModel name: ", MODEL_NAME)
+        # Generate data
+        X, y = self.__data_generation(batch_target_dates)
 
-## Transform Input Sequences ##
+        return X, y
 
-def window_single_spatial_temporal_target(dataset, target_region, target_index, seq_len, sg, step, tar_var=TAR_VAR,
-                                          tar_week=TAR_WEEK, region_emb=True):
-    """  """
-    # Set future target prediction var based on model scenario
-    assert (tar_var in ['temp', 'rf']) & (tar_week in ['34', '56'])
-    # Number of timesteps target is after training history
-    target_size = {'34': 2, '56': 4}[tar_week] * 14 + 1
-    # Column index of target variable
-    target_col = col_names.index(tar_var)
+    def on_epoch_end(self):
+        'Shuffle target_dates after each epoch if shuffle is set to True'
+        if self.shuffle == True:
+            shuffle(self.target_dates)
 
-    # Total length of history
-    history_size = seq_len * step
+    def __data_generation(self, batch_target_dates):
+        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+        # Initialization
+        X = np.empty((self.batch_size, *self.dim))
+        y = np.empty((self.batch_size, self.target_shape))
 
-    # Crop local region
-    local_region = dataset[:, (target_region[0] - sg):(target_region[0] + sg + 1),
-                   (target_region[1] - sg):(target_region[1] + sg + 1), :]
+        # Generate data
+        for i, date in enumerate(batch_target_dates):
+            # Store sample
+            start_date = date-pd.Timedelta(self.horizon+self.lookback, unit='days')
+            end_date = start_date+pd.Timedelta(self.lookback-1, unit='days')
 
-    # Set training indices
-    start_index = target_index - history_size - target_size
-    end_index = target_index - target_size
-    #     print(start_index, end_index, target_index)
-    assert (start_index > 0) & (end_index > 0), "Timestep {} history out of range".format(target_index)
+            X[i,] = self.train.loc[start_date:end_date].to_numpy().reshape(*self.dim)
 
-    indices = range(start_index, end_index, step)
-    # Spatial data
-    spatial_data = local_region[indices, :, :, 3:-4].astype(np.float16)
-    # Temporal data
-    if region_emb:
-        local_reg_id = str((int(local_region[0, sg, sg, 1]), int(local_region[0, sg, sg, 2])))
-        local_reg_id = rgn_id_to_int[local_reg_id]
-        temporal_data = local_region[indices, sg, sg, -4:].astype(np.float16)
-        region_embedding = np.repeat(local_reg_id, seq_len).reshape(seq_len, 1).astype(np.int16)
-        # print(temporal_data.shape, region_embedding.shape)
-    elif not region_emb:
-        temporal_data = local_region[indices, sg, sg, 1:].astype(np.float16)
-    # Target variable
-    target_data = np.array(local_region[target_index, sg, sg, target_col]).astype(np.float16)
+            # Store ground truth
+            y[i] = self.target.loc[date].to_numpy()
 
-    return np.array(spatial_data), np.array(temporal_data), np.array(region_embedding), np.array(target_data)
+        return X, y
 
-
-def generate_input_batch(dataset, batch_size, sg, seq_len, step, tar_var, tar_week):
-    """ """
-    # Set future target prediction var based on model scenario
-    assert (tar_var in ['temp', 'rf']) & (tar_week in ['34', '56'])
-    # Number of timesteps target is after training history
-    target_size = {'34': 2, '56': 4}[tar_week] * 14 + 1
-    # Column index of target variable
-    target_col = col_names.index(tar_var)
-
-    # Initialize lists
-    spatial_data, temporal_data, reg_emb_data, target_data = [], [], [], []
-
-    # Set minimum time index
-    min_time_index = target_size + seq_len * step + 1
-
-    # Generate windowed batch (from random indices and regions)
-    for batch in range(batch_size):
-        # Generate random region & timestep
-        rand_region_idx = np.random.choice(NUM_REGIONS, 1, replace=False)[0]
-        rand_region = target_region_ids[rand_region_idx]
-        rand_date_idx = np.random.choice(range(min_time_index, NUM_TIMESTEPS), 1, replace=False)[0]
-        #         print("Batch number:",batch,"Start Region:",rand_region_idx,"\nStart Region LatLon:",rand_region,"\nStart Timestep:", rand_date_idx)
-        # Generate single window
-        spt, tmp, remb, tar = window_single_spatial_temporal_target(dataset, rand_region, rand_date_idx, seq_len, sg,
-                                                                    step, tar_var, tar_week)
-        spatial_data.append(spt)
-        temporal_data.append(tmp)
-        reg_emb_data.append(remb)
-        target_data.append(tar)
-
-    spatial_data = np.stack(spatial_data)
-    temporal_data = np.stack(temporal_data)
-    reg_emb_data = np.stack(reg_emb_data)
-    target_data = np.stack(target_data)
-
-    yield ({"spatial_input": spatial_data, "temporal_input": temporal_data, "region_id_input": reg_emb_data},
-           {"target_output": target_data})
-
-## Build Model ##
-
-def build_sptl_tmpl_model(seq_len=SEQ_LEN, sptl_width=SPATIAL_WIDTH, sptl_features=SPATIAL_FEATURES,
-                          tmpl_features=TEMPORAL_FEATURES, batch_size=BATCH_SIZE,
-                          sptl_conv_filters=SPTL_CONV_FILTERS, sptl_kernel_size=SPTL_KERNEL_SIZE,
-                          sptl_stride=SPTL_STRIDE, sptl_padding=SPTL_PADDING, sptl_output_dim=SPTL_OUTPUT_DIM,
-                          rgn_id_vocab=NUM_REGIONS, rgn_id_emb_dim=RGN_ID_EMB_DIM, lstm1_units=LSTM1_UNITS,
-                          lstm2_units=LSTM2_UNITS, dropout_rate=DROPOUT_RATE, model_name=MODEL_NAME,
-                          print_summary=True):
-    # Spatial Model Input
-    spatial_input = Input(shape=[seq_len, sptl_width, sptl_width, sptl_features], batch_size=batch_size,
-                          name='spatial_input')
-
-    spatial_model = TimeDistributed(Conv2D(filters=sptl_conv_filters, kernel_size=(sptl_kernel_size, sptl_kernel_size),
-                                           strides=(sptl_stride, sptl_stride), padding=sptl_padding,
-                                           name='sptl_conv2d'), name='td_sptl_conv2d')(spatial_input)
-
-    spatial_model = Flatten(name='sptl_flatten')(spatial_model)
-
-    spatial_model = Reshape(target_shape=(seq_len, -1), name='sptl_reshape')(spatial_model)
-
-    spatial_output = TimeDistributed(Dense(units=sptl_output_dim, activation='relu', name='sptl_emb_out'),
-                                     name='td_sptl_emb_out')(spatial_model)
-
-    # Temporal Model Input
-    temporal_input = Input(shape=(seq_len, tmpl_features), batch_size=batch_size, name='temporal_input')
-
-    # Region ID Input
-    region_id_input = Input(shape=(seq_len, 1), batch_size=batch_size, name='region_id_input')
-
-    region_id_emb = Reshape((seq_len,), input_shape=(seq_len, 1), name='region_id_reshape')(region_id_input)
-
-    region_id_emb = Embedding(input_dim=rgn_id_vocab, input_length=seq_len, mask_zero=False, batch_size=batch_size,
-                              output_dim=rgn_id_emb_dim, name='durations_emb_layer')(region_id_emb)
-
-    # Concat Inputs
-    concat = K.concatenate([spatial_output, temporal_input, region_id_emb], axis=-1)
-
-    # LSTM Architecture
-    lstm_model = LSTM(units=lstm1_units, return_sequences=True, name='lstm_1')(concat)
-
-    lstm_model = LSTM(units=lstm2_units, return_sequences=False, name='lstm_2')(lstm_model)
-
-    model_output = Dense(units=1, name='target_output')(lstm_model)
-
-    model = Model(inputs=[spatial_input, temporal_input, region_id_input], outputs=[model_output], name=model_name)
+def build_model(lookback, train_grid, batch_size, features, target_shape, model_name, print_summary=True):
+    spatial_input = Input(shape=[lookback, *train_grid, len(features)], batch_size=batch_size, name='spatial_input')
+    model = TimeDistributed(Conv2D(filters=16, kernel_size=(2, 2), strides=(1, 1), padding='same', name='conv2d'), name='td_conv2d')(spatial_input)
+    model = Flatten(name='spatial_flatten')(model)
+    model = Reshape(target_shape=(lookback, -1), name='spatial_reshape')(model)
+    model = TimeDistributed(Dense(units=24, activation='relu', name='conv2d_out'), name='td_conv2d_out')(model)
+     # LSTM Architecture
+    lstm_model = LSTM(units=64, return_sequences=True, name='lstm_1')(model)
+    lstm_model = LSTM(units=64, return_sequences=False, name='lstm_2')(lstm_model)
+    model_output = Dense(units=target_shape, name='target_output')(lstm_model)
+    model = Model(inputs=[spatial_input], outputs=[model_output], name=model_name)
 
     if print_summary:
         print(model.summary())
 
     return (model)
 
+if __name__ == '__main__':
+    
+    train_generator = DataGenerator(train=X_train, target=y_train, lookback=LOOKBACK, horizon=HORIZON, train_grid=train_grid, target_shape=target_shape, batch_size=BATCH_SIZE)
 
+    model = build_model(lookback=LOOKBACK, train_grid=train_grid, features=features, target_shape=target_shape, batch_size=BATCH_SIZE, model_name=MODEL_NAME)
 
+    run = wandb.init(project='dissertation',
+           config={
+              "learning_rate": 0.001,
+              "epochs": 25,
+              "batch_size": BATCH_SIZE,
+              "patience": 10,
+              "loss_function": "mean-absolute-error",
+              "architecture": "CNN-LSTM",
+              "dataset": "IMD-IMDAA"
+           })
+    config = wandb.config
 
-def train_spatial_temporal_model(model, dataset_generator, opt='adam', epochs=EPOCHS, steps_per_epoch=STEPS_PER_EPOCH,
-                                 include_tb=False):  # validation_data, val_steps = VALIDATION_STEPS,
-
-    ## Early stopping
-    earlystopping = EarlyStopping(monitor='loss', min_delta=0.00001, patience=10, restore_best_weights=True)  # val_loss
+      ## Early stopping
+    earlystopping = EarlyStopping(monitor='loss', min_delta=0.00001, patience=config.patience, restore_best_weights=True)  # val_loss
 
     # Automatically save latest best model to file
-    filepath = f"models/model_saves/{PRED_TAR}.hdf5"
+    filepath = f"models/model_saves/{TARGET_FEATURE}.hdf5"
     checkpoint = ModelCheckpoint(filepath=filepath, monitor='loss', verbose=0, save_best_only=True, mode='min')
 
     # Set callbacks
-    callbacks_list = [checkpoint, earlystopping]
+    callbacks_list = [checkpoint, earlystopping, WandbCallback()]
 
-    # Include tensorboard
-    if include_tb:
-        tensorboard_cb = tf.keras.callbacks.TensorBoard(f"models/logs")
-        callbacks_list.extend([tensorboard_cb])
+    model.compile(loss='mean_absolute_error', optimizer=Adam(learning_rate=config.learning_rate), metrics=[mae, RootMeanSquaredError(), Huber()])
 
-    # Optimizers
-    optimizers = {'adam': Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=False)}
+    model.fit(train_generator, epochs=config.epochs, use_multiprocessing=True, verbose=1, callbacks=callbacks_list)
 
-    model.compile(loss='mean_absolute_error', optimizer=optimizers[opt],
-                  metrics=[MeanAbsoluteError(), RootMeanSquaredError(), Huber()])
+    test_generator = DataGenerator(train=X_test, target=y_test, lookback=LOOKBACK, horizon=HORIZON, train_grid=train_grid, target_shape=target_shape, batch_size=BATCH_SIZE)
 
-    # Fit model #x = [spatial_train, temporal_train_x], y = temporal_train_y,
-    history = model.fit(dataset_generator, epochs=epochs, use_multiprocessing=True,
-                        # validation_data = validation_data, validation_steps = val_steps,
-                        steps_per_epoch=steps_per_epoch, verbose=1, callbacks=callbacks_list)
-    return (history)
-
-if __name__ == '__main__':
-
-    ## Initiate Generator ##
-    dataset_generator = tf.data.Dataset.from_generator(
-        generator=lambda: generate_input_batch(dataset=global_region_tensor, batch_size=BATCH_SIZE, sg=SG, seq_len=SEQ_LEN, step=STEP, tar_var=TAR_VAR, tar_week=TAR_WEEK),
-        output_types=({"spatial_input":np.float16,"temporal_input":np.float16,"region_id_input":np.int16},{"target_output":np.float16}),
-        output_shapes=({"spatial_input":[BATCH_SIZE,SEQ_LEN,SPATIAL_WIDTH,SPATIAL_WIDTH,SPATIAL_FEATURES], \
-                        "temporal_input":[BATCH_SIZE,SEQ_LEN,TEMPORAL_FEATURES],
-                        "region_id_input":[BATCH_SIZE,SEQ_LEN,1]}, \
-                        {"target_output":[BATCH_SIZE,]}))
-
-    dataset_generator = dataset_generator.prefetch(PREFETCH_SIZE).repeat()
-    # Test
-    for batch in dataset_generator.repeat().take(1):
-        print(batch[0]['spatial_input'].shape, batch[0]['temporal_input'].shape, batch[0]['region_id_input'].shape, batch[1]['target_output'].shape)
-
-    spatial_temporal_model = build_sptl_tmpl_model()
-
-    ## Train ##
-    history = train_spatial_temporal_model(model = spatial_temporal_model, dataset_generator = dataset_generator,
-                                        opt = 'adam', steps_per_epoch=500, include_tb=False)
+    evaluation = model.evaluate(test_generator)
+    print(evaluation)
