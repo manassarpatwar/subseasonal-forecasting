5010481b435d1bcf03f0984a9f35a8f72e3b9635
Manas Sarpatwar <aca18mms@bessemer-node041.shef.ac.uk>
2021-05-19 11:30:29 +0100

    Final commits

--- data/ncmrwf-interpolate.sh
+++ data/ncmrwf-interpolate.sh
@@ -4,12 +4,35 @@
 
 start=1979
 end=2019
+
 for ((i = $start; i <= $end; i++))
 do
-	cdo remapbil,targetgrid ncmrwf/ncum_imdaa_reanl_DY_$1_"$i"0101-"$i"1231.nc interpolated/imdaa_reanl_$1_$i-1x1.nc
+	cdo remapbil,targetgrid ncmrwf/ncum_imdaa_reanl_DY_PRES-sfc_"$i"0101-"$i"1231.nc interpolated/imdaa_reanl_PRES-sfc_$i-1x1.nc
 done
 
-cdo mergetime interpolated/imdaa_reanl_$1_*-1x1.nc interpolated/imdaa_reanl_$1-1x1.nc
-rm interpolated/imdaa_reanl_$1_*-1x1.nc
+cdo mergetime interpolated/imdaa_reanl_PRES-sfc_*-1x1.nc interpolated/imdaa_reanl_PRES-sfc-1x1.nc
+rm interpolated/imdaa_reanl_PRES-sfc_*-1x1.nc
 # Calculate rolling 14 day mean
-RUNSTAT_DATE='last' cdo runmean,14 interpolated/imdaa_reanl_$1-1x1.nc interpolated/imdaa_reanl_$1-1x1-14d.nc
+RUNSTAT_DATE='last' cdo runmean,14 interpolated/imdaa_reanl_PRES-sfc-1x1.nc interpolated/imdaa_reanl_PRES-sfc-1x1-14d.nc
+
+
+for ((i = $start; i <= $end; i++))
+do
+	cdo remapbil,targetgrid ncmrwf/ncum_imdaa_reanl_DY_PRMSL-msl_"$i"0101-"$i"1231.nc interpolated/imdaa_reanl_PRMSL-msl_$i-1x1.nc
+done
+
+cdo mergetime interpolated/imdaa_reanl_PRMSL-msl_*-1x1.nc interpolated/imdaa_reanl_PRMSL-msl-1x1.nc
+rm interpolated/imdaa_reanl_PRMSL-msl_*-1x1.nc
+# Calculate rolling 14 day mean
+RUNSTAT_DATE='last' cdo runmean,14 interpolated/imdaa_reanl_PRMSL-msl-1x1.nc interpolated/imdaa_reanl_PRMSL-msl-1x1-14d.nc
+
+
+for ((i = $start; i <= $end; i++))
+do
+	cdo remapbil,targetgrid ncmrwf/ncum_imdaa_reanl_DY_RH-2m_"$i"0101-"$i"1231.nc interpolated/imdaa_reanl_RH-2m_$i-1x1.nc
+done
+
+cdo mergetime interpolated/imdaa_reanl_RH-2m_*-1x1.nc interpolated/imdaa_reanl_RH-2m-1x1.nc
+rm interpolated/imdaa_reanl_RH-2m_*-1x1.nc
+# Calculate rolling 14 day mean
+RUNSTAT_DATE='last' cdo runmean,14 interpolated/imdaa_reanl_RH-2m-1x1.nc interpolated/imdaa_reanl_RH-2m-1x1-14d.nc
--- download/imd-daily.sh
+++ download/imd-daily.sh
@@ -1,19 +1,48 @@
 #!/bin/bash
-# Request 1 gigabytes of real memory (RAM)
-#SBATCH --mem=1G
-# Request 1 core
-#SBATCH -c 1
-# Email notifications to me@somedomain.com
-#SBATCH --mail-user=mmsarpatwar1@sheffield.ac.uk
-# Email notifications if the job fails
-#SBATCH --mail-type=FAIL
-#SBATCH --mail-type=BEGIN
-#SBATCH --mail-type=END
-#SBATCH --job-name=imd-daily-download
-#SBATCH -o imd-daily.out
+# Author: Manas Sarpatwar
+# Date: 19/05/2021
 
-module load Anaconda3/2019.07
-source activate dissertation
+start=1979
+end=2019
 
-source download/get_imd.sh
+feature="rf" #rf/temp
+variable="rainfall" #rainfall/maxtemp/mintemp
+key="rain" #rain/maxtemp/mintemp
 
+for ((i = $start; i <= $end; i++))
+do
+	curl -X POST -F "$key=$i" https://www.imdpune.gov.in/Clim_Pred_LRF_New/"$variable".php -o data/imd/imd_"$variable"_"$i"0101-"$i"1231.GRD
+	python download/create_ctl.py $feature $i imd_"$variable"_"$i"0101-"$i"1231
+	cdo -f nc import_binary data/imd/imd_"$variable"_"$i"0101-"$i"1231.ctl data/imd/imd_"$variable"_"$i"0101-"$i"1231.nc
+	rm data/imd/imd_"$variable"_"$i"0101-"$i"1231.ctl
+done
+
+cdo mergetime data/imd/imd_"$variable"_*.nc data/imd/imd_"$variable".nc
+
+feature="temp" #rf/temp
+variable="maxtemp" #rainfall/maxtemp/mintemp
+key="maxtemp" #rain/maxtemp/mintemp
+for ((i = $start; i <= $end; i++))
+do
+	curl -X POST -F "$key=$i" https://www.imdpune.gov.in/Clim_Pred_LRF_New/"$variable".php -o data/imd/imd_"$variable"_"$i"0101-"$i"1231.GRD
+	python download/create_ctl.py $feature $i imd_"$variable"_"$i"0101-"$i"1231
+	cdo -f nc import_binary data/imd/imd_"$variable"_"$i"0101-"$i"1231.ctl data/imd/imd_"$variable"_"$i"0101-"$i"1231.nc
+	rm data/imd/imd_"$variable"_"$i"0101-"$i"1231.ctl
+done
+
+cdo mergetime data/imd/imd_"$variable"_*.nc data/imd/imd_"$variable".nc
+
+feature="temp" #rf/temp
+variable="mintemp" #rainfall/maxtemp/mintemp
+key="mintemp" #rain/maxtemp/mintemp
+for ((i = $start; i <= $end; i++))
+do
+	curl -X POST -F "$key=$i" https://www.imdpune.gov.in/Clim_Pred_LRF_New/"$variable".php -o data/imd/imd_"$variable"_"$i"0101-"$i"1231.GRD
+	python download/create_ctl.py $feature $i imd_"$variable"_"$i"0101-"$i"1231
+	cdo -f nc import_binary data/imd/imd_"$variable"_"$i"0101-"$i"1231.ctl data/imd/imd_"$variable"_"$i"0101-"$i"1231.nc
+	rm data/imd/imd_"$variable"_"$i"0101-"$i"1231.ctl
+done
+
+cdo mergetime data/imd/imd_"$variable"_*.nc data/imd/imd_"$variable".nc
+
+cdo ensmean imd_maxtemp.nc imd_mintemp.nc imd_temp.nc
